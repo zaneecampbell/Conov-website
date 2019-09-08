8283cd777b9f59ac710e0b133b659884d6650d4e
Zane Campbell <zanegitinit@gmail.com>
2019-09-08 16:25:56 -0700

    prepared react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Landing />
-    </div>
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
