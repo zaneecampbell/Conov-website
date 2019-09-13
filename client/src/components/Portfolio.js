57cc382e1379643c665200ad0713c6ed05d7e2bd
Zane Campbell <zanegitinit@gmail.com>
2019-09-12 21:50:15 -0700

    began working on segmenting Portfolio images

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 const Portfolio = () => {
   return (
@@ -7,7 +8,9 @@ const Portfolio = () => {
         minHeight: '75vh',
         textAlign: 'center',
         paddingTop: '15px',
-        paddingBottom: '15px'
+        paddingBottom: '15px',
+        maxWidth: '1400px',
+        margin: 'auto'
       }}
     >
       <h1
@@ -16,6 +19,64 @@ const Portfolio = () => {
       >
         Portfolio
       </h1>
+      <div>
+        <Card
+          style={{ maxWidth: '318px', margin: '15px', display: 'inline-block' }}
+        >
+          <CardImg
+            top
+            width='100%'
+            src='/Carousel_Images/1.png'
+            alt='Berong Jusser'
+          />
+          <CardBody>
+            <CardTitle>Berong Jusser</CardTitle>
+            <CardSubtitle>Commission</CardSubtitle>
+          </CardBody>
+        </Card>
+        <Card
+          style={{ maxWidth: '318px', margin: '15px', display: 'inline-block' }}
+        >
+          <CardImg
+            top
+            width='100%'
+            src='/Carousel_Images/2.png'
+            alt='Berong Jusser'
+          />
+          <CardBody>
+            <CardTitle>Berong Jusser</CardTitle>
+            <CardSubtitle>Commission</CardSubtitle>
+          </CardBody>
+        </Card>
+        <Card
+          style={{ maxWidth: '318px', margin: '15px', display: 'inline-block' }}
+        >
+          <CardImg
+            top
+            width='100%'
+            src='/Carousel_Images/3.png'
+            alt='Berong Jusser'
+          />
+          <CardBody>
+            <CardTitle>Berong Jusser</CardTitle>
+            <CardSubtitle>Commission</CardSubtitle>
+          </CardBody>
+        </Card>
+        <Card
+          style={{ maxWidth: '318px', margin: '15px', display: 'inline-block' }}
+        >
+          <CardImg
+            top
+            width='100%'
+            src='/Carousel_Images/4.jpg'
+            alt='Berong Jusser'
+          />
+          <CardBody>
+            <CardTitle>Berong Jusser</CardTitle>
+            <CardSubtitle>Commission</CardSubtitle>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from 'reactstrap';
 
 const Type = () => {
   return (
-    <div style={{ paddingBottom: '30px' }}>
+    <div style={{ paddingBottom: '30px', marginTop: '50px' }}>
       <Container style={{ textAlign: 'center' }} fluid>
         <h1
           style={{ marginBottom: '25px', fontSize: '4rem' }}
@@ -29,7 +29,8 @@ const Type = () => {
             <strong>
               <h3>Type A</h3>
               <span style={{ fontSize: '.85rem' }}>
-                <div>Half body 110＄/ Full body 150 ＄ (Tax incl.)</div>
+                <div>Half body $110/ Full body $150 (Tax incl.)</div>
+                <div>Extra Props $5-$30</div>
                 <div>Simple style</div>
                 <div>Simple shading/ Highlights</div>
               </span>
@@ -39,7 +40,8 @@ const Type = () => {
             <strong>
               <h3>Type B</h3>
               <span style={{ fontSize: '.85rem' }}>
-                <div>Half body 130＄/ Full body 170 ＄ (Tax incl.)</div>
+                <div>Half body $130/ Full body $170 (Tax incl.)</div>
+                <div>Extra Props $5-$30</div>
                 <div>Illustration style</div>
                 <div>More color then Type A</div>
               </span>
@@ -49,7 +51,8 @@ const Type = () => {
             <strong>
               <h3>Type C</h3>
               <span style={{ fontSize: '.85rem' }}>
-                <div>Half body 200＄/ Full body 240 ＄ (Tax incl.)</div>
+                <div>Half body $200/ Full body $240 (Tax incl.)</div>
+                <div>Extra Props $10-$50</div>
                 <div>Full style</div>
                 <div>Vibrant shading and highlights</div>
               </span>
